66b30519d22440a01049c0bf14b937a5e79518e4
oz <zaleambo@gmail.com>
2026-02-25 19:34:28 +0300

    solution

---
+++ A_Segment_with_Small_Sum.py
@@ -0,0 +1,11 @@
+n , s = [int(x) for x in input().split()]
+arr = [int(x) for x in input().split()]
+
+ans , curr , l = 0 ,0 ,0
+for r , num in enumerate(arr):
+    curr += num
+    while curr >= s:
+        curr -= arr[l]
+        l += 1
+    ans = max(ans,r -l + 1)
+print(ans)
