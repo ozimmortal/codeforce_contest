f8a5c476863f9b44174fa4866a797c504401320e
oz <zaleambo@gmail.com>
2026-02-27 21:09:36 +0300

    solved

---
+++ D_Number_of_Segments_with_Big_Sum.py
@@ -0,0 +1,15 @@
+n , s = [int(x) for x in input().split()]
+nums = [int(x) for x in input().split()]
+
+l ,res = 0 , 0
+curr = 0
+
+for r in range(n):
+    curr += nums[r]
+    while curr >= s:
+        curr -= nums[l]
+        l += 1
+
+        res += n - r
+
+print(res)
