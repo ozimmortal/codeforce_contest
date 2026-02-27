82b23e6078bd4f42e1d974dc14c1ca1f933df8af
oz <zaleambo@gmail.com>
2026-02-27 21:53:06 +0300

    solved

--- B_Most_socially_distanced_subsequence.py
+++ B_Most_socially_distanced_subsequence.py
@@ -2,4 +2,16 @@ t = int(input())
 
 for _ in range(t):
     n = int(input())
-    arr = []
+    arr = [int(x) for x in input().split()]
+
+    ans = [arr[0]]
+
+    for  i in range(1,n-1):
+        if (arr[i] - arr[i -1]) * (arr[i + 1] - arr[i]) < 0:
+            ans.append(arr[i])
+    ans.append(arr[-1])
+    print(len(ans))
+    print(*ans)
+
+
+    
