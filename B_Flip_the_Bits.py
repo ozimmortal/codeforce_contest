005d4a2ab3825046af0305d18476c3b4ac14c5c7
oz <zaleambo@gmail.com>
2026-04-06 13:37:24 +0300

    solved

--- B_Flip_the_Bits.py
+++ B_Flip_the_Bits.py
@@ -1,18 +1,38 @@
 t = int(input())
-
 for _ in range(t):
-    n = int(input())
-    a = input()
-    b = input()
-
     
-
-    ans = True
-
-    for r in range(n):
-        pass
+    n = int(input())
+    a = input().strip()
+    b = input().strip()
+    
+    balance = [0] * n
+    ones = 0
+    zeros = 0
+    
+    for i in range(n):
+        if a[i] == '1':
+            ones += 1
+        else:
+            zeros += 1
+        balance[i] = ones - zeros
+    
+    flipped = False
+    possible = True
+    
+    for i in range(n - 1, -1, -1):
+        current = a[i]
         
-            
-
-
-    print("YES" if ans else "NO")
+        if flipped:
+            current = '1' if current == '0' else '0'
+        
+        if current == b[i]:
+            continue
+        
+        # need to flip prefix [0..i]
+        if balance[i] != 0:
+            possible = False
+            break
+        
+        flipped = not flipped
+    
+    print("YES" if possible else "NO")
